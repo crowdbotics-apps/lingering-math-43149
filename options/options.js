30274a634f50fd7410a1064df7a08ca01a6b9788
Crowdbotics <team@crowdbotics.com>
2023-08-30 15:49:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lingering-math-43149",
+  url: "https://lingering-math-43149.botics.co",
+  api: "https://lingering-math-43149.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
